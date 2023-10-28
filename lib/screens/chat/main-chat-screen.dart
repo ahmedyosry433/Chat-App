d46f2d97fef139384c0fc98dad59d7a5b154748c
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2023-10-28 10:51:01 +0200

    update splash screen

--- lib/screens/chat/main-chat-screen.dart
+++ lib/screens/chat/main-chat-screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:chat_app/core/theme/app-colors/app-colors-light.dart';
 import 'package:chat_app/screens/chat/components/appbar-chat-main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,96 +16,125 @@ class ChatMain extends StatefulWidget {
 }
 
 class _ChatMainState extends State<ChatMain> {
+  bool isLoading = false;
+  final user = FirebaseAuth.instance.currentUser;
+  @override
+  void initState() {
+    getUsersFromFirestore();
+    super.initState();
+  }
+  getUsersFromFirestore() {
+    if (!isLoading) {
+      context.read<AuthProvider>().getUserByUid(user!.uid);
+      context.read<AuthProvider>().getUsersFromFirestore();
+      isLoading = true;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final subAuthProvider = Provider.of<AuthProvider>(context);
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: [
-          const AppbarMainChat(),
-          SizedBox(
-            height: 100,
-            child: Expanded(
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: subAuthProvider.allUsersFormFirebase.length,
-                  itemBuilder: ((context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 50,
-                            width: 50,
-                            child: CircleAvatar(),
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                  '${subAuthProvider.allUsersFormFirebase[index].firstName} ',
-                                  style: const TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600)),
-                              Text(
-                                  ' ${subAuthProvider.allUsersFormFirebase[index].lastName}',
-                                  style: const TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600)),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  })),
-            ),
-          ),
-          const Divider(
-            color: AppColorLight.sceondColor,
-          ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: subAuthProvider.allUsersFormFirebase.length,
-                itemBuilder: ((context, index) {
-                  return SizedBox(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              const SizedBox(
-                                  height: 50, width: 50, child: CircleAvatar()),
-                              const SizedBox(width: 10),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+      body: !isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                const AppbarMainChat(),
+                SizedBox(
+                  height: 100,
+                  child: Expanded(
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: subAuthProvider.allUsersFormFirebase.length,
+                        itemBuilder: ((context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                  child: CircleAvatar(),
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                        '${subAuthProvider.allUsersFormFirebase[index].firstName} ',
+                                        style: const TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w600)),
+                                    Text(
+                                        ' ${subAuthProvider.allUsersFormFirebase[index].lastName}',
+                                        style: const TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w600)),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        })),
+                  ),
+                ),
+                const Divider(
+                  color: AppColorLight.sceondColor,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: subAuthProvider.allUsersFormFirebase.length,
+                      itemBuilder: ((context, index) {
+                        return GestureDetector(
+                          onTap: () =>
+                              Navigator.pushNamed(context, '/chatdetails'),
+                          child: SizedBox(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text(
-                                      '${subAuthProvider.allUsersFormFirebase[index].firstName} ${subAuthProvider.allUsersFormFirebase[index].lastName}',
-                                      style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w700)),
-                                  const Text(
-                                    'Last Message',
-                                    style: TextStyle(
-                                        fontSize: 11, color: Colors.grey),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                          height: 50,
+                                          width: 50,
+                                          child: CircleAvatar()),
+                                      const SizedBox(width: 10),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                              '${subAuthProvider.allUsersFormFirebase[index].firstName} ${subAuthProvider.allUsersFormFirebase[index].lastName}',
+                                              style: const TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w700)),
+                                          const Text(
+                                            'Last Message',
+                                            style: TextStyle(
+                                                fontSize: 11,
+                                                color: Colors.grey),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
                                   ),
+                                  const Text('10:25 am'),
                                 ],
                               ),
-                            ],
+                            ),
                           ),
-                          const Text('10:25 am'),
-                        ],
-                      ),
-                    ),
-                  );
-                })),
-          ),
-        ],
-      ),
+                        );
+                      })),
+                ),
+              ],
+            ),
     ));
   }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../provider/auth-provider.dart';
 import 'auth/login-screen.dart';
 import 'chat/main-chat-screen.dart';
 
@@ -14,21 +12,6 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-  bool isLoading = false;
-  final user = FirebaseAuth.instance.currentUser;
-  getUsersFromFirestore() {
-    if (!isLoading) {
-      context.read<AuthProvider>().getUserByUid(user!.uid);
-      context.read<AuthProvider>().getUsersFromFirestore();
-    }
-  }
-
-  @override
-  void initState() {
-    getUsersFromFirestore();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,11 +19,7 @@ class _SplashState extends State<Splash> {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : const ChatMain();
+            return const ChatMain();
           } else {
             return LoginPage();
           }
